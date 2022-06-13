4d16f1bb5aa33e4c10dd27e5ec622bd86dcacd67
Ricardo Ferraz <sourvivens@gmail.com>
2022-06-13 15:55:49 -0300

    aula13 e 14

---
+++ projeto01/aula13/repeticoesLacos.js
@@ -0,0 +1,18 @@
+// Laço de repetiçao WHILE - Testa primeiro, executa o bloco depois
+// MAIS USADO
+var c = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++
+}
+
+// Laço de repetiço Do / while - Executa o bloco e depois testa
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} 
+while (c <= 14)
+
+
+
---
+++ projeto01/aula14/for.js
@@ -0,0 +1,7 @@
+// Laço de repetição 'for' (para).
+
+console.log('Vai começar')
+for(var c = 1; c <= 10; c++) {
+    console.log(c)
+} 
+console.log('Fim!')
